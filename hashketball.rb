4c69c1dc132a6f0562db302ce9d2a101ea0b93f3
Tyler Nodell <tyler.l.nodell@gmail.com>
2018-07-12 16:43:32 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -139,7 +139,7 @@ end
 def team_colors(teamname)
   game_hash.each { |team, teamdata|
     if teamdata[:name] == teamname
-        return team[:colors]
+        return team_name[:colors]
     end
   }
 end
