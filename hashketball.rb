caf5437bbd207e990136abf4e59fce99fe32355c
Tyler Nodell <tyler.l.nodell@gmail.com>
2018-07-12 17:01:10 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -11,7 +11,7 @@ def game_hash
           shoe: 16,
           points: 22,
           rebounds: 12,
-          assist: 12,
+          assists: 12,
           steals: 3,
           blocks: 1,
           slam_dunks: 1
@@ -21,7 +21,7 @@ def game_hash
           shoe: 14,
           points: 12,
           rebounds: 12,
-          assist: 12,
+          assists: 12,
           steals: 12,
           blocks: 12,
           slam_dunks: 7
@@ -31,7 +31,7 @@ def game_hash
           shoe: 17,
           points: 17,
           rebounds: 19,
-          assist: 10,
+          assists: 10,
           steals: 3,
           blocks: 1,
           slam_dunks: 15
@@ -41,7 +41,7 @@ def game_hash
           shoe: 19,
           points: 26,
           rebounds: 12,
-          assist: 6,
+          assists: 6,
           steals: 3,
           blocks: 8,
           slam_dunks: 5
@@ -51,7 +51,7 @@ def game_hash
           shoe: 15,
           points: 19,
           rebounds: 2,
-          assist: 2,
+          assists: 2,
           steals: 4,
           blocks: 11,
           slam_dunks: 1
@@ -67,7 +67,7 @@ def game_hash
           shoe: 18,
           points: 10,
           rebounds: 1,
-          assist: 1,
+          assists: 1,
           steals: 2,
           blocks: 7,
           slam_dunks: 2
@@ -77,7 +77,7 @@ def game_hash
           shoe: 16,
           points: 12,
           rebounds: 4,
-          assist: 7,
+          assists: 7,
           steals: 7,
           blocks: 15,
           slam_dunks: 10
@@ -87,7 +87,7 @@ def game_hash
           shoe: 14,
           points: 24,
           rebounds: 12,
-          assist: 12,
+          assists: 12,
           steals: 4,
           blocks: 5,
           slam_dunks: 5
@@ -97,7 +97,7 @@ def game_hash
           shoe: 15,
           points: 33,
           rebounds: 3,
-          assist: 2,
+          assists: 2,
           steals: 1,
           blocks: 1,
           slam_dunks: 0
@@ -107,7 +107,7 @@ def game_hash
           shoe: 15,
           points: 6,
           rebounds: 12,
-          assist: 12,
+          assists: 12,
           steals: 22,
           blocks: 5,
           slam_dunks: 12
