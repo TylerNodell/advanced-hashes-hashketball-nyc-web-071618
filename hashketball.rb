744e5f1f60549908cb960edd7174d6481c624fdf
Tyler Nodell <tyler.l.nodell@gmail.com>
2018-07-12 16:16:44 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -118,7 +118,8 @@ end
 
 def num_points_scored(playername)
   game_hash.each { |team, data|
-    data[:players].each { |player, data|
+    players = data[:players]
+    players.each { |player, data|
       if player == playername
         data.fetch(:points)
       end
